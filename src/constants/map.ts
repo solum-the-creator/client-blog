a3a407af443927c5fee768132c2930f896a8ff62
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-11-27 18:41:05 +0300

    feat: add map component to Contact page

---
+++ src/constants/map.ts
@@ -0,0 +1,17 @@
+export const mapMarkers = [
+  {
+    id: 'minsk',
+    longitude: 27.561247,
+    latitude: 53.902156,
+  },
+  {
+    id: 'vilnus',
+    longitude: 25.270528,
+    latitude: 54.689378,
+  },
+  {
+    id: 'warsaw',
+    longitude: 21.006672,
+    latitude: 52.232282,
+  },
+];
